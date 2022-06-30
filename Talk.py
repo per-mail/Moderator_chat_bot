6afe033480a66947cb74b2c7db4e3216e3abe59e
per-mail <per-mail@mail.ru>
2022-06-30 22:13:02 +0700

    commit 30.06.2022

--- Talk.py
+++ Talk.py
@@ -1,3 +1,4 @@
+#видео: https://live.skillbox.ru/webinars/code/znakomimsya-s-python-i-arkhitekturoi-umnogo-chat-bota200622/
 from random import choice
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -11,40 +12,45 @@ from multiprocessing.sharedctypes import Value
 import re
 import nltk
 
-
+#форматируем очищаем текст 
 def filter_text(text):
-    text = text.lower()
-    pattern = r'[^\w\s]'
-    text = re.sub(pattern, "", text)
+    text = text.lower()# приводим текст к нижнему регистру
+    pattern = r'[^\w\s]'# Регулярное выражение "Всё что не слово и не пробел"
+    # \w\s -(слово или пробел) w(слово) s(пробел)
+    # ^ - отрицание
+    # [...]- символьная группа
+    text = re.sub(pattern, "", text)# заменяем в тексте "Всё что не слово и не пробел" на "" 
     return text
 
-
+#форматируем текст 
 def is_match(text1, text2):
-    text1 = filter_text(text1)
-    text2 = filter_text(text2)
+    text1 = filter_text(text1)# текст пользователя
+    text2 = filter_text(text2)# ответ бота
     
 #проверяем чтобы текст не был пустым
     if len(text1) == 0 or len(text2) == 0:
         return False
-
+    
+#смотрим содержатся ли слова (фразы) из базы в вводимом тексте
     if text1.find(text2) != -1:
         return True
-
+#смотрим содержатся ли слова (фразы) из вводимого текста в базе
     if text2.find(text1) != -1:
         return True
-
+    
+#работа с опечатками, смотрим насколько вводимые слова отличаются от исходных
     distance = nltk.edit_distance(text1, text2)
     length = (len(text1) + len(text2))/2
     score = distance / length
 
-    return score < 0.1
+    return score < 0.1# если разница меньше 10% то слова равны
 
-
+#создаём список вопросов и ответов - интенты
 BOT_CONFIG = {
     "intents": {
         "Hello": {
             "examples": ["Привет", "Здарова", "Оу", "Приветос", "Хеллоу"],
-            "responses": ["Здравствуй, человек", "Привет", "Ооо, привет"]
+            "responses": ["Здравствуй, человек", "Привет", "Ооо, привет"] #интент
         },
         "how _are_you?": {
             "examples": ["Как дела?", "Как жизнь?", "Как поживаешь?"],
@@ -89,9 +95,9 @@ BOT_CONFIG = {
     }
 }
 
-
+#классификация текстов. Ищем в базе подходящий интент на вводимую фразу
 def printAnswer(text, examples, responses):
-    for example in examples:
+    for example in examples: 
         if is_match(text, example):
             print(random.choice(responses))
             break
@@ -100,30 +106,32 @@ def printAnswer(text, examples, responses):
 config_file = open(
     "C:\\Users\\dfg\Desktop\\Python\\Skillbox_data_sience\\New\\big_bot_config.json", "r")
 
-BIG_CONFIG = json.load(config_file)
+BIG_CONFIG = json.load(config_file)#загружаем интенты в BIG_CONFIG
 BIG_CONFIG.keys()
 len(BIG_CONFIG["intents"])
-X = []
-y = []
+X = []#вводимая фраза
+y = []#ответная фраза (интент)
 
-for name, intent in BIG_CONFIG["intents"].items():
-    for example in intent["examples"]:
+#находим нужный интент по фразе
+for name, intent in BIG_CONFIG["intents"].items():# пробегаемся по каждому интенту
+    for example in intent["examples"]:#пробегаемся по examples
+        X.append(example)# ответы из интентов
+        y.append(name)# name-название интента
+    for example in intent["responses"]:#пробегаемся по responses
         X.append(example)
         y.append(name)
-    for example in intent["responses"]:
-        X.append(example)
-        y.append(name)
+        
+# векторизация перевод текста в набор чисел
+vectorizer = CountVectorizer()#создаём векторайзер
+vectorizer.fit(X)# обучаем векторайзер
+model = RandomForestClassifier()#Создаём модель
+vecX = vectorizer.transform(X)#Преобразуем тексты в вектора
+model.fit(vecX, y)#Обучаем модель
 
-vectorizer = CountVectorizer()
-vectorizer.fit(X)
-model = RandomForestClassifier()
-vecX = vectorizer.transform(X)
-model.fit(vecX, y)
-
-
+#функция которая будет работать с моделью
 def get_intent_ml(text):
-    vec_text = vectorizer.transform([text])
-    intent = model.predict(vec_text)[0]
+    vec_text = vectorizer.transform([text])#векторизуем текст
+    intent = model.predict(vec_text)[0]#предсказывает ответный интент, берём первый элемент[0]
     return intent
 
 
@@ -138,10 +146,10 @@ def get_intent(text):
 
 def speak(phrase):
     phrase = filter_text(phrase)
-    intent = get_intent(phrase)
-    if not intent:
-        intent = get_intent_ml(phrase)
-    if intent:
+    intent = get_intent(phrase)#ищем интент
+    if not intent:# если намерение не найдено
+        intent = get_intent_ml(phrase)# пробуем подключить модель
+    if intent:# если нашелся интент выводим его из BIG_CONFIG
         responses = BIG_CONFIG["intents"][intent]["responses"]
         return random.choice(responses)
     failure = BIG_CONFIG["failure_phrases"]
@@ -150,7 +158,7 @@ def speak(phrase):
 #фразы для выхода
 #msg = ""
 #exit_pharases = ["Выйти", "Выключись", "Пока", "пока", "давай", "Давай"]
-#while not msg in exit_pharases:
+#while not msg in exit_pharases:#пока нет фразы для выхода
 #    msg = input()
 #    print("[BOT]" + speak(msg))
         
--- handlers/empty.py
+++ handlers/empty.py
@@ -51,11 +51,10 @@ async def filter_message(message: types.Message, state: FSMContext):
                 await bot.send_message(message.from_user.id, f'{message.from_user.first_name}. Вы получили ограничения за нарушение правил!') 
  
 # общение с ботом мпортируем функцию speak и передаём в неё message.text из чата
-# проверяем
-       # else:
-             #reply = speak(message.text)
-             #await message.reply(speak(message.text))
-             #await bot.send_message(GROUP_ID, reply)
+        else:
+             reply = speak(message.text)
+             await message.reply(speak(message.text))
+             await bot.send_message(GROUP_ID, reply)
              
             
 # Вариант закрытая группа(вход по приглашению) приветствие, удаленние записи, внесение в базу, не впускаем пользователей которые в чёрном списке
